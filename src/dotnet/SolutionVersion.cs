427b112a587cd05eb5415c2907d336364b56f30b
scott-gibson <scott.gibson@exacttarget.com>
2015-04-24 15:52:19 -0400

    version up 3.3.4.9

--- src/dotnet/SolutionVersion.cs
+++ src/dotnet/SolutionVersion.cs
@@ -5,10 +5,9 @@ using System.Security;
 [assembly: AssemblyDescription("ZooKeeper Client for .NET http://zookeeper.apache.org")]
 [assembly: AssemblyProduct("ZooKeeperNet")]
 [assembly: AssemblyCopyright("Copyright 2007-2012 ewhauser, omwok. - All rights reserved.")]
-[assembly: AssemblyVersion("3.3.4.8")]
-[assembly: AssemblyFileVersion("3.3.4.8")]
-
-[assembly: AssemblyInformationalVersion("3.3.4.8")]
+[assembly: AssemblyVersion("3.3.4.9")]
+[assembly: AssemblyFileVersion("3.3.4.9")]
+[assembly: AssemblyInformationalVersion("3.3.4.9")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(false)]
 
