cecec1751e74a8e0eb181d8596da9154ea5c0e18
Damien Diederen <dd@crosstwine.com>
2021-04-15 15:07:00 +0200

    ZOOKEEPER-2454: Cleanup: Hold enforced schemes in a Java Set

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/AuthenticationHelper.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/AuthenticationHelper.java
@@ -18,9 +18,10 @@
 package org.apache.zookeeper.server;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.data.Id;
@@ -42,7 +43,7 @@ public class AuthenticationHelper {
     public static final String SASL_AUTH_SCHEME = "sasl";
 
     private boolean enforceAuthEnabled;
-    private List<String> enforceAuthSchemes = new ArrayList<>();
+    private final Set<String> enforceAuthSchemes = new HashSet<>();
     private boolean saslAuthRequired;
 
     public AuthenticationHelper() {
